5dcca8903b424c34ea8e582d1b692de3119d289d
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-01-21 11:29:40 +0100

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,18 @@
+
+/*** Mini-Challenge  */
+// Satzbau + Punktuation
+
+// Vereinbarung:
+// "S" (sentence) --> "."  Ich bin Max.
+// "Q" (question) --> "?"  Bist du Max?
+// "E" (exclamation) --> "!"  Ich bin!
+
+// Parameter --> funktion getSentence([ ...], "Q"){ ...}
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
